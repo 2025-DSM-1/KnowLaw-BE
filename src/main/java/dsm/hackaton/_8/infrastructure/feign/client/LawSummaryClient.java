40cdb2c85fcb735d85a482e483e51295853116a8
coehgns <modooboiroo@gmail.com>
2025-07-15 22:34:21 +0900

    feat: LawSummaryClient

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawSummaryClient.java
@@ -0,0 +1,14 @@
+package dsm.hackaton._8.infrastructure.feign.client;
+
+import dsm.hackaton._8.infrastructure.feign.dto.request.LawSummaryRequest;
+import dsm.hackaton._8.infrastructure.feign.dto.response.LawSummaryResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "lawSummaryClient", url = "${python.api.url}")
+public interface LawSummaryClient {
+
+    @PostMapping("/law/summary")
+    LawSummaryResponse summarizeLaw(@RequestBody LawSummaryRequest request);
+}
