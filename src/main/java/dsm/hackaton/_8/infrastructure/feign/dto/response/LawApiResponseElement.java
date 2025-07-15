72f3ecb80c3ef3cbeb16bb5e816655427d86a710
coehgns <modooboiroo@gmail.com>
2025-07-16 00:24:17 +0900

    feat: LawApiResponseElement

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/response/LawApiResponseElement.java
@@ -0,0 +1,37 @@
+package dsm.hackaton._8.infrastructure.feign.dto.response;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "Law")
+public class LawApiResponseElement {
+
+    @JacksonXmlProperty(localName = "billNo")
+    private int lawSerialNumber; // 의안번호 (Proposal Number)
+
+    @JacksonXmlProperty(localName = "billName")
+    private String lawTitle; // 의안명 (Proposal Name)
+
+    @JacksonXmlProperty(localName = "proposeDt")
+    private LocalDate propositionDate; // 제안 일자 (Proposal Date) or 발의일
+
+    @JacksonXmlProperty(localName = "generalResult")
+    private String lawResult; // 의결 결과 (Resolution Result)
+
+    @JacksonXmlProperty(localName = "summary")
+    private String mainContent; // 주요 내용 (Key Contents)
+
+    @JacksonXmlProperty(localName = "procStageCd")
+    private String lawStatus; // 심사 진행 상태 (Review Progress Status)
+
+//    @JacksonXmlProperty(localName = "")
+//    private String procDt;  // 의결 일자 (Resolution Date)
+}
