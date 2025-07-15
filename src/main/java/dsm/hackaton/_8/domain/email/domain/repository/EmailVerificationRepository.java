dd144af02280c80125e45fe8d326ef90660a4f42
coehgns <modooboiroo@gmail.com>
2025-07-15 19:39:11 +0900

    feat: EmailVerificationRepository에 existsByEmail 추가

--- src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
 
     Optional<EmailVerification> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
