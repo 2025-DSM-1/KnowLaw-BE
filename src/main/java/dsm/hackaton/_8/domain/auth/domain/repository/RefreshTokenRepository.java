23a57665aaf8f4aa844f4d4d4266f8101c568f78
coehgns <modooboiroo@gmail.com>
2025-07-15 20:32:53 +0900

    feat: RefreshTokenRepository에 findByToken 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,9 @@ package dsm.hackaton._8.domain.auth.domain.repository;
 import dsm.hackaton._8.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
 }
