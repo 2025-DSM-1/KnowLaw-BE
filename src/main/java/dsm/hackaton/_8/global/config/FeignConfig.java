0d847dbefd3df5730530d28e1945d9c278a6e891
coehgns <modooboiroo@gmail.com>
2025-07-15 22:55:51 +0900

    feat: FeignConfig에 feignDecoder 추가

--- src/main/java/dsm/hackaton/_8/global/config/FeignConfig.java
+++ src/main/java/dsm/hackaton/_8/global/config/FeignConfig.java
@@ -1,13 +1,21 @@
 package dsm.hackaton._8.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import dsm.hackaton._8.infrastructure.feign.FeignClientErrorDecoder;
 import feign.Logger;
+import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.beans.factory.ObjectFactory;
 
 @EnableFeignClients(basePackages = "dsm.hackaton._8.global.config")
 @Configuration
@@ -23,5 +31,13 @@ public class FeignConfig {
     @ConditionalOnMissingBean(value = ErrorDecoder.class)
     public FeignClientErrorDecoder commonFeignErrorDecoder() {
         return new FeignClientErrorDecoder();
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        ObjectMapper objectMapper = new XmlMapper(); // XML 전용 ObjectMapper
+        HttpMessageConverter<?> converter = new MappingJackson2XmlHttpMessageConverter(objectMapper);
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(converter);
+        return new SpringDecoder(objectFactory);
     }
 }
