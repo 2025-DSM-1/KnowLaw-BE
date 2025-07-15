d9d3150864e5c1ff0824e25c753014c77ee54f14
coehgns <modooboiroo@gmail.com>
2025-07-15 20:56:56 +0900

    feat: QueryLawDetailResponse

---
+++ src/main/java/dsm/hackaton/_8/domain/law/presentatoin/dto/response/QueryLawDetailResponse.java
@@ -0,0 +1,30 @@
+package dsm.hackaton._8.domain.law.presentatoin.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QueryLawDetailResponse {
+
+    private Long lawId;
+
+    private String lawTitle;
+
+    private List<LawSummaryContentResponse> lawSummaryContent;
+
+    private String lawStatus;
+
+    private String propositionDate;
+
+    private String backgroundInfo;
+
+    private String example;
+}
