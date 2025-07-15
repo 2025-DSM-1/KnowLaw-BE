26671f69c32e1fbffad60add1228dc612ade1f7b
coehgns <modooboiroo@gmail.com>
2025-07-15 16:58:20 +0900

    feat: ErrorCode

---
+++ src/main/java/dsm/hackaton/_8/global/error/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package dsm.hackaton._8.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    USER_NOT_FOUND(404, "user not found");
+
+    private final int statusCode;
+    private final String message;
+}
