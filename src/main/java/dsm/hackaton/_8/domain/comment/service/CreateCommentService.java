4d99a2672d7b1e11954a861f880902004010c4cb
coehgns <modooboiroo@gmail.com>
2025-07-16 02:23:55 +0900

    feat: CreateCommentService

---
+++ src/main/java/dsm/hackaton/_8/domain/comment/service/CreateCommentService.java
@@ -0,0 +1,39 @@
+package dsm.hackaton._8.domain.comment.service;
+
+import dsm.hackaton._8.domain.comment.domain.Comment;
+import dsm.hackaton._8.domain.comment.domain.repository.CommentRepository;
+import dsm.hackaton._8.domain.comment.presentaion.dto.request.CreateCommentRequest;
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.law.domain.repository.LawRepository;
+import dsm.hackaton._8.domain.law.exception.LawNotFoundException;
+import dsm.hackaton._8.domain.user.domain.User;
+import dsm.hackaton._8.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CreateCommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final UserFacade userFacade;
+
+    private final LawRepository lawRepository;
+
+    @Transactional
+    public void execute(CreateCommentRequest request, Long lawId) {
+        User user = userFacade.getCurrentUser();
+        Law law = lawRepository.findById(lawId).orElseThrow(() -> LawNotFoundException.EXCEPTION);
+
+        Comment comment = Comment.builder()
+                .commentType(request.getCommentType())
+                .content(request.getContent())
+                .userId(user)
+                .lawId(law)
+                .build();
+
+        commentRepository.save(comment);
+    }
+}
