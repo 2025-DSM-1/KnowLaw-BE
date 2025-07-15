a327d96359e9d46c0a56eb7202b05bf77cabd18f
coehgns <modooboiroo@gmail.com>
2025-07-15 17:29:12 +0900

    refactor: EmailVerificationRepository

--- src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
@@ -3,5 +3,5 @@ package dsm.hackaton._8.domain.email.domain.repository;
 import dsm.hackaton._8.domain.email.domain.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailVerificationRepository extends JpaRepository<Long, EmailVerification> {
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
 }
