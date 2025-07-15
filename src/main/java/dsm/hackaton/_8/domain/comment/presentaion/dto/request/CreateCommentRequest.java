1b2736ea1fe162f1695e839a070b8553af59f531
coehgns <modooboiroo@gmail.com>
2025-07-16 03:32:02 +0900

    refactor: CreateCommentRequest 유효성 검증 추가

--- src/main/java/dsm/hackaton/_8/domain/comment/presentaion/dto/request/CreateCommentRequest.java
+++ src/main/java/dsm/hackaton/_8/domain/comment/presentaion/dto/request/CreateCommentRequest.java
@@ -1,12 +1,16 @@
 package dsm.hackaton._8.domain.comment.presentaion.dto.request;
 
 import dsm.hackaton._8.domain.comment.domain.type.CommentType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CreateCommentRequest {
 
+    @NotNull(message = "commentType은 필수입니다.")
     private CommentType commentType;
 
+    @NotBlank(message = "content는 공백일 수 없습니다.")
     private String content;
 }
