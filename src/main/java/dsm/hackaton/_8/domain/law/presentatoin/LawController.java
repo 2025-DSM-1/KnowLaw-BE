84fc9974062148871738dd035ac13bca5552a358
coehgns <modooboiroo@gmail.com>
2025-07-16 00:41:02 +0900

    feat: LawController에 queryLawDetailResponse 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/law/presentatoin/LawController.java
+++ src/main/java/dsm/hackaton/_8/domain/law/presentatoin/LawController.java
@@ -1,9 +1,12 @@
 package dsm.hackaton._8.domain.law.presentatoin;
 
 import dsm.hackaton._8.domain.law.presentatoin.dto.response.QueryAllLawsResponse;
+import dsm.hackaton._8.domain.law.presentatoin.dto.response.QueryLawDetailResponse;
 import dsm.hackaton._8.domain.law.service.QueryAllLawsService;
+import dsm.hackaton._8.domain.law.service.QueryLawDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,15 @@ public class LawController {
 
     private final QueryAllLawsService queryAllLawsService;
 
+    private final QueryLawDetailService queryLawDetailService;
+
     @GetMapping
     public QueryAllLawsResponse queryAllLaws() {
         return queryAllLawsService.execute();
+    }
+
+    @GetMapping("{law-id}")
+    public QueryLawDetailResponse queryLawDetailResponse(@PathVariable("law-id") Long lawId) {
+        return queryLawDetailService.execute(lawId);
     }
 }
