a4bc50761a0d9931f9b5247ab3921f234c2c9c1c
coehgns <modooboiroo@gmail.com>
2025-07-16 08:56:01 +0900

    feat: Favorite

---
+++ src/main/java/dsm/hackaton/_8/domain/favorite/domain/Favorite.java
@@ -0,0 +1,35 @@
+package dsm.hackaton._8.domain.favorite.domain;
+
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "law_id", nullable = false)
+    private Law law;
+}
