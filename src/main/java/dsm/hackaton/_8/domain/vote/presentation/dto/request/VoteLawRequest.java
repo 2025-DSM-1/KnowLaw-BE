d78d40c1d8a2a52119f46b7189fd7a263ffbc8d3
coehgns <modooboiroo@gmail.com>
2025-07-16 03:31:49 +0900

    refactor: VoteLawRequest 유효성 검증 추가

--- src/main/java/dsm/hackaton/_8/domain/vote/presentation/dto/request/VoteLawRequest.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/presentation/dto/request/VoteLawRequest.java
@@ -1,10 +1,12 @@
 package dsm.hackaton._8.domain.vote.presentation.dto.request;
 
 import dsm.hackaton._8.domain.vote.domain.type.VoteType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class VoteLawRequest {
 
+    @NotNull(message = "voteType은 필수입니다.")
     private VoteType voteType;
 }
