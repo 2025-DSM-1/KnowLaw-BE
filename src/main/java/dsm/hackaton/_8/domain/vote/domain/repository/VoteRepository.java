6a97331f93a3d0c83b1907237996c498f62475c7
coehgns <modooboiroo@gmail.com>
2025-07-16 02:38:23 +0900

    feat: VoteRepository에 findVoteByLawId 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/vote/domain/repository/VoteRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/domain/repository/VoteRepository.java
@@ -4,4 +4,6 @@ import dsm.hackaton._8.domain.vote.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    Vote findVoteByLawId(Long lawId);
 }
