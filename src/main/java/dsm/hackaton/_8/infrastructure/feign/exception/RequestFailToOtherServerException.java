b242a863eb21d717e415d54d82ce4dc65b54e6a4
coehgns <modooboiroo@gmail.com>
2025-07-15 21:10:29 +0900

    feat: RequestFailToOtherServerException

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/exception/RequestFailToOtherServerException.java
@@ -0,0 +1,13 @@
+package dsm.hackaton._8.infrastructure.feign.exception;
+
+import dsm.hackaton._8.global.error.exception.DSMException;
+import dsm.hackaton._8.global.error.exception.ErrorCode;
+
+public class RequestFailToOtherServerException extends DSMException {
+
+    public static final DSMException EXCEPTION = new RequestFailToOtherServerException();
+
+    private RequestFailToOtherServerException() {
+        super(ErrorCode.REQUEST_FAIL_TO_OTHER_SERVER);
+    }
+}
