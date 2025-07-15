1186dea376b6a820d92497ddeeee7cd54252b997
coehgns <modooboiroo@gmail.com>
2025-07-16 08:56:05 +0900

    feat: FavoriteRepository

---
+++ src/main/java/dsm/hackaton/_8/domain/favorite/domain/repository/FavoriteRepository.java
@@ -0,0 +1,11 @@
+package dsm.hackaton._8.domain.favorite.domain.repository;
+
+import dsm.hackaton._8.domain.favorite.domain.Favorite;
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+    Favorite findByLawAndUser(Law law, User user);
+}
