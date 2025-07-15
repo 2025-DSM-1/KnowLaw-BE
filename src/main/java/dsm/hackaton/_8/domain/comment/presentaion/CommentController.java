f1d8f90923c3dae02ec29ae53ced7f15525f62bb
coehgns <modooboiroo@gmail.com>
2025-07-16 03:32:23 +0900

    refactor: CommentController @Valid 추가

--- src/main/java/dsm/hackaton/_8/domain/comment/presentaion/CommentController.java
+++ src/main/java/dsm/hackaton/_8/domain/comment/presentaion/CommentController.java
@@ -4,10 +4,12 @@ import dsm.hackaton._8.domain.comment.presentaion.dto.request.CreateCommentReque
 import dsm.hackaton._8.domain.comment.presentaion.dto.response.QueryAllCommentsResponse;
 import dsm.hackaton._8.domain.comment.service.CreateCommentService;
 import dsm.hackaton._8.domain.comment.service.QueryAllCommentsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class CommentController {
     private final QueryAllCommentsService queryAllCommentsService;
 
     @PostMapping("/{law-id}")
-    public void createComment(@PathVariable("law-id") Long lawId, CreateCommentRequest request) {
+    public void createComment(@PathVariable("law-id") Long lawId, @RequestBody @Valid CreateCommentRequest request) {
         createCommentService.execute(request, lawId);
     }
 
