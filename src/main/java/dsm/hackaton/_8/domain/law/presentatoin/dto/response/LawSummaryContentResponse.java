54537d0c9511f67af979ecf079df883d9d930fbe
coehgns <modooboiroo@gmail.com>
2025-07-15 20:56:50 +0900

    feat: LawSummaryContentResponse

---
+++ src/main/java/dsm/hackaton/_8/domain/law/presentatoin/dto/response/LawSummaryContentResponse.java
@@ -0,0 +1,15 @@
+package dsm.hackaton._8.domain.law.presentatoin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LawSummaryContentResponse {
+    private String summaryElement;
+}
+
