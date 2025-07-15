414380815edc747f4c1b0cd6a69e566478186340
coehgns <modooboiroo@gmail.com>
2025-07-15 22:36:16 +0900

    refactor: 폴더 구조 수정

--- src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawOpenApiClient.java
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawOpenApiClient.java
@@ -1,6 +1,6 @@
 package dsm.hackaton._8.infrastructure.feign.client;
 
-import dsm.hackaton._8.infrastructure.feign.dto.LawApiResponse;
+import dsm.hackaton._8.infrastructure.feign.dto.response.LawApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
