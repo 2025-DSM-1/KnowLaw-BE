686dd341ea5fe32b37d2856d8565a96a25112ab9
coehgns <modooboiroo@gmail.com>
2025-07-15 16:38:55 +0900

    feat: RefreshToken

---
+++ src/main/java/dsm/hackaton/_8/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,23 @@
+package dsm.hackaton._8.domain.auth.domain;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@Builder
+@RedisHash
+public class RefreshToken {
+
+    @Id
+    private String email;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long ttl;
+}
