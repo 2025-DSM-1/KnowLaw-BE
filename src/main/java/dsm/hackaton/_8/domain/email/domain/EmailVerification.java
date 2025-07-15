5cfc51d1b893773be330de0e339aa0973d0ca031
coehgns <modooboiroo@gmail.com>
2025-07-15 16:50:50 +0900

    feat: EmailVerification

---
+++ src/main/java/dsm/hackaton/_8/domain/email/domain/EmailVerification.java
@@ -0,0 +1,34 @@
+package dsm.hackaton._8.domain.email.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "auth_code", nullable = false)
+    private String authCode;
+
+    @Column(name = "is_verified", nullable = false)
+    private Boolean isVerified = false;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
