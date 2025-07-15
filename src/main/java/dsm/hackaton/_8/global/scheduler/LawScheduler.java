613f1b78adb48e4dbd59806c3d4033ce8d24ba53
coehgns <modooboiroo@gmail.com>
2025-07-16 00:23:44 +0900

    feat: LawScheduler

---
+++ src/main/java/dsm/hackaton/_8/global/scheduler/LawScheduler.java
@@ -0,0 +1,18 @@
+package dsm.hackaton._8.global.scheduler;
+
+import dsm.hackaton._8.domain.law.service.SaveLawsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LawScheduler {
+
+    private SaveLawsService saveLawsService;
+
+    @Scheduled(cron = "0 */5 * * * ?")
+    public void saveLawsEveryFiveMinutes() {
+        saveLawsService.execute();
+    }
+}
