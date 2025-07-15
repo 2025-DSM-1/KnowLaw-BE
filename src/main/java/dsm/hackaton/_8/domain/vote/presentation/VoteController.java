6095bb893516104c04646b32b36a356d0031f4ca
coehgns <modooboiroo@gmail.com>
2025-07-16 03:32:30 +0900

    feat: VoteController @Valid 추가

--- src/main/java/dsm/hackaton/_8/domain/vote/presentation/VoteController.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/presentation/VoteController.java
@@ -4,10 +4,12 @@ import dsm.hackaton._8.domain.vote.presentation.dto.request.VoteLawRequest;
 import dsm.hackaton._8.domain.vote.presentation.dto.response.VoteLawResponse;
 import dsm.hackaton._8.domain.vote.service.QueryVoteStatusService;
 import dsm.hackaton._8.domain.vote.service.VoteLawService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class VoteController {
     private final QueryVoteStatusService queryVoteStatusService;
 
     @PostMapping("/{law-id}")
-    public void voteLaw(@PathVariable("law-id") Long lawId, VoteLawRequest request) {
+    public void voteLaw(@PathVariable("law-id") Long lawId, @RequestBody @Valid VoteLawRequest request) {
         voteLawService.execute(request, lawId);
     }
 
