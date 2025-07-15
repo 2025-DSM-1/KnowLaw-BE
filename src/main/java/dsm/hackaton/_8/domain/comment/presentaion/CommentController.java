f5503b0865920aef05cf93c405231135c414581f
coehgns <modooboiroo@gmail.com>
2025-07-16 02:23:45 +0900

    feat: CommentController

---
+++ src/main/java/dsm/hackaton/_8/domain/comment/presentaion/CommentController.java
@@ -0,0 +1,32 @@
+package dsm.hackaton._8.domain.comment.presentaion;
+
+import dsm.hackaton._8.domain.comment.presentaion.dto.request.CreateCommentRequest;
+import dsm.hackaton._8.domain.comment.presentaion.dto.response.QueryAllCommentsResponse;
+import dsm.hackaton._8.domain.comment.service.CreateCommentService;
+import dsm.hackaton._8.domain.comment.service.QueryAllCommentsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CreateCommentService createCommentService;
+
+    private final QueryAllCommentsService queryAllCommentsService;
+
+    @PostMapping("/{law-id}")
+    public void createComment(@PathVariable("law-id") Long lawId, CreateCommentRequest request) {
+        createCommentService.execute(request, lawId);
+    }
+
+    @GetMapping
+    public QueryAllCommentsResponse queryAllComments() {
+        return queryAllCommentsService.execute();
+    }
+}
