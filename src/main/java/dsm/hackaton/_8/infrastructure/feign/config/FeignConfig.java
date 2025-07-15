76a4300213a6f77d6754f06d4f6a554c3c3333d6
coehgns <modooboiroo@gmail.com>
2025-07-15 21:12:05 +0900

    feat: FeignConfig

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/config/FeignConfig.java
@@ -0,0 +1,27 @@
+package dsm.hackaton._8.infrastructure.feign.config;
+
+import dsm.hackaton._8.infrastructure.feign.FeignClientErrorDecoder;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@EnableFeignClients(basePackages = "dsm.hackaton._8.infrastructure.feign.config")
+@Configuration
+@Import(FeignClientErrorDecoder.class)
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = ErrorDecoder.class)
+    public FeignClientErrorDecoder commonFeignErrorDecoder() {
+        return new FeignClientErrorDecoder();
+    }
+}
