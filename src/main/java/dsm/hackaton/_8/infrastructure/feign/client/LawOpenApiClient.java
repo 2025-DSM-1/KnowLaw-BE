42fb4184ffe4b464f3bd35f39ef313ec2bb8bd62
coehgns <modooboiroo@gmail.com>
2025-07-15 23:30:51 +0900

    feat: LawOpenApiClient에 configuration 추가

--- src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawOpenApiClient.java
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawOpenApiClient.java
@@ -1,12 +1,14 @@
 package dsm.hackaton._8.infrastructure.feign.client;
 
+import dsm.hackaton._8.global.config.FeignConfig;
 import dsm.hackaton._8.infrastructure.feign.dto.response.LawApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(
         name = "lawOpenApiClient",
-        url = "${openApi.law.url}"
+        url = "${openApi.law.url}",
+        configuration = FeignConfig.class
 )
 public interface LawOpenApiClient {
 
