5a675bc964266fb540940c0a23f20c3d9bc0c32a
coehgns <modooboiroo@gmail.com>
2025-07-15 21:39:47 +0900

    feat: LawOpenApiClient

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/client/LawOpenApiClient.java
@@ -0,0 +1,15 @@
+package dsm.hackaton._8.infrastructure.feign.client;
+
+import dsm.hackaton._8.infrastructure.feign.dto.LawApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "lawOpenApiClient",
+        url = "${openApi.law.url}"
+)
+public interface LawOpenApiClient {
+
+    @GetMapping
+    LawApiResponse getLaws();
+}
