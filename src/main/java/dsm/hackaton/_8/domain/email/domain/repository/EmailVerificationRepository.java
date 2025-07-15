0352e9b726b6b3115c39f868adcf675b307c8a51
coehgns <modooboiroo@gmail.com>
2025-07-15 19:39:00 +0900

    feat: EmailVerificationRepository에 findByEmail 추가

--- src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/email/domain/repository/EmailVerificationRepository.java
@@ -3,5 +3,9 @@ package dsm.hackaton._8.domain.email.domain.repository;
 import dsm.hackaton._8.domain.email.domain.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+
+    Optional<EmailVerification> findByEmail(String email);
 }
