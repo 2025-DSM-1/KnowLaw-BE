7268dbf9ba2bb25ed3b760b4607580ce0963d17c
coehgns <modooboiroo@gmail.com>
2025-07-16 02:44:26 +0900

    feat: VoteController에 queryVoteStatus 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/vote/presentation/VoteController.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/presentation/VoteController.java
@@ -1,8 +1,11 @@
 package dsm.hackaton._8.domain.vote.presentation;
 
 import dsm.hackaton._8.domain.vote.presentation.dto.request.VoteLawRequest;
+import dsm.hackaton._8.domain.vote.presentation.dto.response.VoteLawResponse;
+import dsm.hackaton._8.domain.vote.service.QueryVoteStatusService;
 import dsm.hackaton._8.domain.vote.service.VoteLawService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +18,15 @@ public class VoteController {
 
     private final VoteLawService voteLawService;
 
+    private final QueryVoteStatusService queryVoteStatusService;
+
     @PostMapping("/{law-id}")
     public void voteLaw(@PathVariable("law-id") Long lawId, VoteLawRequest request) {
         voteLawService.execute(request, lawId);
+    }
+
+    @GetMapping("/{law-id}")
+    public VoteLawResponse queryVoteStatus(@PathVariable("law-id") Long lawId) {
+        return queryVoteStatusService.execute(lawId);
     }
 }
