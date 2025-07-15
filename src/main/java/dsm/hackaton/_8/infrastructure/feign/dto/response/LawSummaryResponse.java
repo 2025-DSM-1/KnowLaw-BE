be20fbca3d29e84548c89051a64197b510f250bc
coehgns <modooboiroo@gmail.com>
2025-07-15 22:34:30 +0900

    feat: LawSummaryResponse

---
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/response/LawSummaryResponse.java
@@ -0,0 +1,16 @@
+package dsm.hackaton._8.infrastructure.feign.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LawSummaryResponse {
+    private String lawSummary;
+    private String backgroundInfo;
+    private String example;
+}
