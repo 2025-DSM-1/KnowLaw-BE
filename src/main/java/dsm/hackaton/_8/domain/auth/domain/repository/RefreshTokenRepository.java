88f8e2e7cf5bda1688effec21556c82600d8067f
coehgns <modooboiroo@gmail.com>
2025-07-15 17:28:47 +0900

    refactor: RefreshTokenRepository

--- src/main/java/dsm/hackaton/_8/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,5 @@ package dsm.hackaton._8.domain.auth.domain.repository;
 import dsm.hackaton._8.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<Long, RefreshToken> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
