5981f08106429bb3c2f3639f633577293dda6706
coehgns <modooboiroo@gmail.com>
2025-07-16 02:24:57 +0900

    feat: VoteRepository

---
+++ src/main/java/dsm/hackaton/_8/domain/vote/domain/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package dsm.hackaton._8.domain.vote.domain.repository;
+
+import dsm.hackaton._8.domain.vote.domain.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
