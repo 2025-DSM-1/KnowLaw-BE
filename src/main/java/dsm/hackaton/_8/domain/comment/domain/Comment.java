9efcf065ea6b2a0be44a484ea990b5a197e8d284
coehgns <modooboiroo@gmail.com>
2025-07-16 02:23:35 +0900

    feat: Comment

---
+++ src/main/java/dsm/hackaton/_8/domain/comment/domain/Comment.java
@@ -0,0 +1,47 @@
+package dsm.hackaton._8.domain.comment.domain;
+
+import dsm.hackaton._8.domain.comment.domain.type.CommentType;
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private CommentType commentType;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    private User userId;
+
+    @ManyToOne
+    private Law lawId;
+}
