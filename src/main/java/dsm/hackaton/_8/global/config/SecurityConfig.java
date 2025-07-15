b44f1c11147cc222e27752961f800057c37e4a83
coehgns <modooboiroo@gmail.com>
2025-07-15 17:31:58 +0900

    feat: SecurityConfig

---
+++ src/main/java/dsm/hackaton/_8/global/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package dsm.hackaton._8.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dsm.hackaton._8.global.security.jwt.JwtProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(authorize -> authorize
+
+                        .requestMatchers("/auth/**")
+                        .permitAll()
+
+                        .requestMatchers("/email/**")
+                        .permitAll()
+
+                        .anyRequest()
+                        .authenticated())
+
+                .with(new FilterConfig(jwtProvider, objectMapper), Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
