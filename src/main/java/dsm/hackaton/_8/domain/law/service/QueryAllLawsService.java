e1d0c81c2cc27ca93221faa4963cbc251b66e079
coehgns <modooboiroo@gmail.com>
2025-07-15 20:53:50 +0900

    feat: QueryAllLawsService

---
+++ src/main/java/dsm/hackaton/_8/domain/law/service/QueryAllLawsService.java
@@ -0,0 +1,38 @@
+package dsm.hackaton._8.domain.law.service;
+
+import dsm.hackaton._8.domain.law.domain.repository.LawRepository;
+import dsm.hackaton._8.domain.law.presentatoin.dto.response.LawResponse;
+import dsm.hackaton._8.domain.law.presentatoin.dto.response.QueryAllLawsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAllLawsService {
+
+    private final LawRepository lawRepository;
+
+    @Transactional(readOnly = true)
+    public QueryAllLawsResponse execute() {
+        List<LawResponse> laws = lawRepository.findAll()
+                .stream()
+                .map(law -> LawResponse.builder()
+                        .lawId(law.getId())
+                        .lawSerialNumber(law.getLawSerialNumber())
+                        .lawTitle(law.getLawTitle())
+                        .lawContent(law.getLawContent())
+                        .promulgationDate(law.getPromulgationDate())
+                        .enforcementDate(law.getEnforcementDate())
+                        .build())
+                .collect(Collectors.toList());
+
+        return QueryAllLawsResponse.builder()
+                .laws(laws)
+                .build();
+    }
+
+}
