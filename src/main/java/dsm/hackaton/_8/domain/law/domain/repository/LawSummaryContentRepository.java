719ab8130b95151710f3793702a0dfcac79529ee
coehgns <modooboiroo@gmail.com>
2025-07-16 12:24:37 +0900

    feat: LawSummaryContentRepository

---
+++ src/main/java/dsm/hackaton/_8/domain/law/domain/repository/LawSummaryContentRepository.java
@@ -0,0 +1,7 @@
+package dsm.hackaton._8.domain.law.domain.repository;
+
+import dsm.hackaton._8.domain.law.domain.LawSummaryContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawSummaryContentRepository extends JpaRepository<LawSummaryContent, Long> {
+}
