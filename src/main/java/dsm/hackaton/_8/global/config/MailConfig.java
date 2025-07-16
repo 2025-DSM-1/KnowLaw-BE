50cc30b28446e550a74f536e92371196eab6245e
coehgns <modooboiroo@gmail.com>
2025-07-16 16:34:58 +0900

    refactor: MailConfig

--- src/main/java/dsm/hackaton/_8/global/config/MailConfig.java
+++ src/main/java/dsm/hackaton/_8/global/config/MailConfig.java
@@ -1,6 +1,5 @@
 package dsm.hackaton._8.global.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
