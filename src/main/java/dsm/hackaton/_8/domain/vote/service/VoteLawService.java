d9c252b7b8b45c5d02359e080547ed7a77e8fbee
coehgns <modooboiroo@gmail.com>
2025-07-17 05:41:07 +0900

    feat: VoteLawService

--- src/main/java/dsm/hackaton/_8/domain/vote/service/VoteLawService.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/service/VoteLawService.java
@@ -1,6 +1,13 @@
 package dsm.hackaton._8.domain.vote.service;
 
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.law.domain.repository.LawRepository;
+import dsm.hackaton._8.domain.law.exception.LawNotFoundException;
+import dsm.hackaton._8.domain.user.domain.User;
+import dsm.hackaton._8.domain.user.facade.UserFacade;
+import dsm.hackaton._8.domain.vote.domain.UserVote;
 import dsm.hackaton._8.domain.vote.domain.Vote;
+import dsm.hackaton._8.domain.vote.domain.repository.UserVoteRepository;
 import dsm.hackaton._8.domain.vote.domain.repository.VoteRepository;
 import dsm.hackaton._8.domain.vote.presentation.dto.request.VoteLawRequest;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +20,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class VoteLawService {
 
     private final VoteRepository voteRepository;
+    private final UserFacade userFacade;
+    private final LawRepository lawRepository;
+    private final UserVoteRepository userVoteRepository;
 
     @Transactional
     public void execute(VoteLawRequest request, Long lawId) {
+        User user = userFacade.getCurrentUser();
         Vote vote = voteRepository.findVoteByLawId(lawId);
+        Law law = lawRepository.findById(lawId).orElseThrow(() -> LawNotFoundException.EXCEPTION);
+
+        UserVote userVote = UserVote.builder()
+                .user(user)
+                .law(law)
+                .voteType(request.getVoteType())
+                .build();
 
         switch (request.getVoteType()) {
             case AGREE:
@@ -24,5 +42,7 @@ public class VoteLawService {
             default:
                 vote.incrementDisAgree();
         }
+
+        userVoteRepository.save(userVote);
     }
 }
