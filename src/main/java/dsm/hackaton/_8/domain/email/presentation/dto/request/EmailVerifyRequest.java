f255c0d85eca31330587ab8e278b11a7ebd0c46c
coehgns <modooboiroo@gmail.com>
2025-07-16 16:33:34 +0900

    refactor: EmailVerifyRequest

--- src/main/java/dsm/hackaton/_8/domain/email/presentation/dto/request/EmailVerifyRequest.java
+++ src/main/java/dsm/hackaton/_8/domain/email/presentation/dto/request/EmailVerifyRequest.java
@@ -1,4 +1,4 @@
-package dsm.hackaton._8.domain.email.presentation.dto.requset;
+package dsm.hackaton._8.domain.email.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
