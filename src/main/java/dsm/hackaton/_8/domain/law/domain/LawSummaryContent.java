858fa93675d1834cd2b2d19426e61dcef2f7c4c6
coehgns <modooboiroo@gmail.com>
2025-07-16 12:25:41 +0900

    feat: LawSummaryContent

--- src/main/java/dsm/hackaton/_8/domain/law/domain/LawSummaryContent.java
+++ src/main/java/dsm/hackaton/_8/domain/law/domain/LawSummaryContent.java
@@ -5,8 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class LawSummaryContent {
 
     @Id
