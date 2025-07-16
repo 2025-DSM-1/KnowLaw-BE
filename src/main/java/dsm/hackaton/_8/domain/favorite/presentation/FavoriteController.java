ada026dad7c1ec3c64f5e87386e2447bc40389d1
coehgns <modooboiroo@gmail.com>
2025-07-16 09:29:22 +0900

    feat: FavoriteController에 queryFavoriteLaw 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/favorite/presentation/FavoriteController.java
+++ src/main/java/dsm/hackaton/_8/domain/favorite/presentation/FavoriteController.java
@@ -1,9 +1,12 @@
 package dsm.hackaton._8.domain.favorite.presentation;
 
+import dsm.hackaton._8.domain.favorite.presentation.dto.response.QueryFavoriteLawResponse;
 import dsm.hackaton._8.domain.favorite.service.AddFavoriteService;
 import dsm.hackaton._8.domain.favorite.service.DeleteFavoriteService;
+import dsm.hackaton._8.domain.favorite.service.QueryFavoriteLawService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,8 @@ public class FavoriteController {
 
     private final DeleteFavoriteService deleteFavoriteService;
 
+    private final QueryFavoriteLawService queryFavoriteLawService;
+
     @PostMapping("/{law-id}")
     public void addFavorite(@PathVariable("law-id") Long lawId) {
         addFavoriteService.execute(lawId);
@@ -26,5 +31,10 @@ public class FavoriteController {
     @DeleteMapping("/{law-id}")
     public void deleteFavorite(@PathVariable("law-id") Long lawId) {
         deleteFavoriteService.deleteFavorite(lawId);
+    }
+
+    @GetMapping
+    public QueryFavoriteLawResponse queryFavoriteLaw() {
+        return queryFavoriteLawService.execute();
     }
 }
