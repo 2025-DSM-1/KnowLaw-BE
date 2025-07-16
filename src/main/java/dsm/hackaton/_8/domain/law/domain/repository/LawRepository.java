be5ea91e43d5a1bccf13c28282eee05b51ece382
coehgns <modooboiroo@gmail.com>
2025-07-16 09:57:42 +0900

    feat: LawRepository에 findByLawSerialNumber 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/law/domain/repository/LawRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/law/domain/repository/LawRepository.java
@@ -3,5 +3,9 @@ package dsm.hackaton._8.domain.law.domain.repository;
 import dsm.hackaton._8.domain.law.domain.Law;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LawRepository extends JpaRepository<Law, Long> {
+
+    Optional<Law> findByLawSerialNumber(int serialNumber);
 }
