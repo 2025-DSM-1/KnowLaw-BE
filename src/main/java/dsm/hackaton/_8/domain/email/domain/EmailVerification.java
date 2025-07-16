77d2893a6705c610b5d74d555a4384ce1d2f6be2
coehgns <modooboiroo@gmail.com>
2025-07-16 16:33:26 +0900

    refactor: EmailVerification

--- src/main/java/dsm/hackaton/_8/domain/email/domain/EmailVerification.java
+++ src/main/java/dsm/hackaton/_8/domain/email/domain/EmailVerification.java
@@ -5,14 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Entity
+@Getter
 @NoArgsConstructor
 public class EmailVerification {
 
