544b60e4faa6f7758bd2df4812620aeb6f278144
coehgns <modooboiroo@gmail.com>
2025-07-17 03:08:22 +0900

    refactor: TokenRefreshService

--- src/main/java/dsm/hackaton/_8/domain/auth/service/TokenRefreshService.java
+++ src/main/java/dsm/hackaton/_8/domain/auth/service/TokenRefreshService.java
@@ -1,7 +1,11 @@
 package dsm.hackaton._8.domain.auth.service;
 
+import dsm.hackaton._8.domain.auth.domain.RefreshToken;
+import dsm.hackaton._8.domain.auth.domain.repository.RefreshTokenRepository;
+import dsm.hackaton._8.domain.auth.exceptoin.RefreshTokenNotFoundException;
 import dsm.hackaton._8.domain.auth.presentation.dto.response.TokenResponse;
-import dsm.hackaton._8.global.security.jwt.TokenRefreshUtil;
+import dsm.hackaton._8.global.security.jwt.JwtProperties;
+import dsm.hackaton._8.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +13,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TokenRefreshService {
 
-    private final TokenRefreshUtil tokenRefreshUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtProvider jwtProvider;
+    private final JwtProperties jwtProperties;
 
-    public TokenResponse execute(String refreshToken) {
-        return tokenRefreshUtil.tokenRefresh(refreshToken);
+    public TokenResponse execute(String token) {
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+
+        TokenResponse tokens = jwtProvider.generateToken(refreshToken.getEmail());
+        refreshToken.updateToken(tokens.getRefreshToken(), jwtProperties.getRefreshExp());
+
+        return tokens;
     }
 }
