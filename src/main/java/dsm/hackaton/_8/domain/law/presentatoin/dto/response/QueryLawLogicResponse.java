0b09c16d1632d257b409867dcd08fde1adfb96ad
coehgns <modooboiroo@gmail.com>
2025-07-16 13:06:02 +0900

    refactor: QueryLawLogicResponse

--- src/main/java/dsm/hackaton/_8/domain/law/presentatoin/dto/response/QueryLawLogicResponse.java
+++ src/main/java/dsm/hackaton/_8/domain/law/presentatoin/dto/response/QueryLawLogicResponse.java
@@ -1,7 +1,11 @@
 package dsm.hackaton._8.domain.law.presentatoin.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 @Builder
 public class QueryLawLogicResponse {
 
