0f425a5f9548ac1de42ee093c8abe8a479f80947
coehgns <modooboiroo@gmail.com>
2025-07-16 12:09:02 +0900

    refactor: LawSummaryResponse

--- src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/response/LawSummaryResponse.java
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/response/LawSummaryResponse.java
@@ -1,14 +1,18 @@
 package dsm.hackaton._8.infrastructure.feign.dto.response;
 
+import dsm.hackaton._8.domain.law.presentatoin.dto.response.LawSummaryContentResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LawSummaryResponse {
-    private String lawSummary;
+    private String lawContent;
+    private List<LawSummaryContentResponse> lawSummaryContent;
     private String backgroundInfo;
     private String example;
     private String agreeLogic;
