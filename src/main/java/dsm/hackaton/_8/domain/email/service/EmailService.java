3fabefa9da4c9217fa47edcf013a3e3500f6e751
coehgns <modooboiroo@gmail.com>
2025-07-16 09:58:10 +0900

    refactor: EmailService에 senEmail 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/email/service/EmailService.java
+++ src/main/java/dsm/hackaton/_8/domain/email/service/EmailService.java
@@ -11,12 +11,19 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendEmail(String to, String authCode) {
+    public void sendAuthCode(String to, String authCode) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setSubject("Email Verification Code");
-        message.setText("Your verification code is: " + authCode);
+        message.setSubject("이메일 인증 코드");
+        message.setText("인증코드: " + authCode);
+        mailSender.send(message);
+    }
 
+    public void sendEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
         mailSender.send(message);
     }
 }
