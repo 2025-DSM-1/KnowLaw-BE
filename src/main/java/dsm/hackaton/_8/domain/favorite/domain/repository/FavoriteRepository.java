61fbe940df496372b003edb5a50be1b8c1241032
coehgns <modooboiroo@gmail.com>
2025-07-16 09:28:11 +0900

    feat: FavoriteRepository에 findAllByUser 메서드 추가

--- src/main/java/dsm/hackaton/_8/domain/favorite/domain/repository/FavoriteRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/favorite/domain/repository/FavoriteRepository.java
@@ -5,7 +5,11 @@ import dsm.hackaton._8.domain.law.domain.Law;
 import dsm.hackaton._8.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
     Favorite findByLawAndUser(Law law, User user);
+
+    List<Favorite> findAllByUser(User user);
 }
