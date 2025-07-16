4e09115164685b07e1bab5ba47baf90a4ce6f499
coehgns <modooboiroo@gmail.com>
2025-07-16 11:08:02 +0900

    refactor: SignUpRequest

--- src/main/java/dsm/hackaton/_8/domain/auth/presentation/dto/request/SignUpRequest.java
+++ src/main/java/dsm/hackaton/_8/domain/auth/presentation/dto/request/SignUpRequest.java
@@ -16,9 +16,6 @@ public class SignUpRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotBlank(message = "인증 코드는 필수입니다.")
-    private String authCode;
-
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
     @Pattern(
