eafd722e31dee99bb01cd589692872b6978b91a7
coehgns <modooboiroo@gmail.com>
2025-07-17 13:12:40 +0900

    refactor: UserVoteRepository

--- src/main/java/dsm/hackaton/_8/domain/vote/domain/repository/UserVoteRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/vote/domain/repository/UserVoteRepository.java
@@ -1,10 +1,13 @@
 package dsm.hackaton._8.domain.vote.domain.repository;
 
+import dsm.hackaton._8.domain.law.domain.Law;
 import dsm.hackaton._8.domain.user.domain.User;
 import dsm.hackaton._8.domain.vote.domain.UserVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserVoteRepository extends JpaRepository<UserVote, Long> {
 
-    UserVote findUserVoteByUser(User user);
+    UserVote findByUserId(Long userId);
+
+    Boolean existsByUserAndLaw(User user, Law law);
 }
