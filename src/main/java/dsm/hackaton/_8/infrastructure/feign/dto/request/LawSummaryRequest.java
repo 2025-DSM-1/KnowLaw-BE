f65afd752b3cd24a2432a2f80a6b5d8363b7f3f1
coehgns <modooboiroo@gmail.com>
2025-07-17 13:12:21 +0900

    refactor: LawSummaryRequest

--- src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/request/LawSummaryRequest.java
+++ src/main/java/dsm/hackaton/_8/infrastructure/feign/dto/request/LawSummaryRequest.java
@@ -1,5 +1,6 @@
 package dsm.hackaton._8.infrastructure.feign.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LawSummaryRequest {
+
+    @NotBlank(message = "lawModifiedContent는 필수입니다.")
     private String lawModifiedContent;
 }
