03c61fb5ba285a17b187673f702263659dbb9b66
coehgns <modooboiroo@gmail.com>
2025-07-17 13:12:31 +0900

    refactor: QueryAllCommentsService

--- src/main/java/dsm/hackaton/_8/domain/comment/service/QueryAllCommentsService.java
+++ src/main/java/dsm/hackaton/_8/domain/comment/service/QueryAllCommentsService.java
@@ -4,10 +4,9 @@ import dsm.hackaton._8.domain.comment.domain.Comment;
 import dsm.hackaton._8.domain.comment.domain.repository.CommentRepository;
 import dsm.hackaton._8.domain.comment.presentaion.dto.response.CommentResponseElement;
 import dsm.hackaton._8.domain.comment.presentaion.dto.response.QueryAllCommentsResponse;
-import dsm.hackaton._8.domain.user.domain.User;
-import dsm.hackaton._8.domain.user.facade.UserFacade;
-import dsm.hackaton._8.domain.vote.domain.UserVote;
-import dsm.hackaton._8.domain.vote.domain.repository.UserVoteRepository;
+import dsm.hackaton._8.domain.law.domain.Law;
+import dsm.hackaton._8.domain.law.domain.repository.LawRepository;
+import dsm.hackaton._8.domain.law.exception.LawNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,22 +19,18 @@ import java.util.stream.Collectors;
 public class QueryAllCommentsService {
 
     private final CommentRepository commentRepository;
-
-    private final UserVoteRepository userVoteRepository;
-
-    private final UserFacade userFacade;
+    private final LawRepository lawRepository;
 
     @Transactional(readOnly = true)
-    public QueryAllCommentsResponse execute() {
-        List<Comment> comments = commentRepository.findAll();
-        User user = userFacade.getCurrentUser();
-        UserVote userVote = userVoteRepository.findUserVoteByUser(user);
+    public QueryAllCommentsResponse execute(Long lawId) {
+        Law law = lawRepository.findById(lawId).orElseThrow(() -> LawNotFoundException.EXCEPTION);
+        List<Comment> comments = commentRepository.findAllByLawId(law.getId());
 
         List<CommentResponseElement> responseElements = comments.stream()
                 .map(comment -> CommentResponseElement.builder()
                         .commentId(comment.getId())
                         .commentType(comment.getCommentType())
-                        .voteType(userVote.getVoteType())
+                        .voteType(comment.getVoteType())
                         .content(comment.getContent())
                         .author(comment.getUser().getName())
                         .build())
