2ca9412b2c2b8081d961afc9d7a50ea9f217338e
coehgns <modooboiroo@gmail.com>
2025-07-17 13:10:55 +0900

    refactor: CommentRepository

--- src/main/java/dsm/hackaton/_8/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/dsm/hackaton/_8/domain/comment/domain/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package dsm.hackaton._8.domain.comment.domain.repository;
 
 import dsm.hackaton._8.domain.comment.domain.Comment;
+import dsm.hackaton._8.domain.law.domain.Law;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByLawId(Long LawId);
 }
