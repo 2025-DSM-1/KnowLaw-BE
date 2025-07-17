017fc034e60bc83b18a4f7f9f6168827e8c788f7
coehgns <modooboiroo@gmail.com>
2025-07-17 13:10:59 +0900

    refactor: CreateCommentService

--- src/main/java/dsm/hackaton/_8/domain/comment/service/CreateCommentService.java
+++ src/main/java/dsm/hackaton/_8/domain/comment/service/CreateCommentService.java
@@ -8,6 +8,8 @@ import dsm.hackaton._8.domain.law.domain.repository.LawRepository;
 import dsm.hackaton._8.domain.law.exception.LawNotFoundException;
 import dsm.hackaton._8.domain.user.domain.User;
 import dsm.hackaton._8.domain.user.facade.UserFacade;
+import dsm.hackaton._8.domain.vote.domain.UserVote;
+import dsm.hackaton._8.domain.vote.domain.repository.UserVoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,14 +23,17 @@ public class CreateCommentService {
     private final CommentRepository commentRepository;
     private final UserFacade userFacade;
     private final LawRepository lawRepository;
+    private final UserVoteRepository userVoteRepository;
 
     @Transactional
     public void execute(CreateCommentRequest request, Long lawId) {
         User user = userFacade.getCurrentUser();
         Law law = lawRepository.findById(lawId).orElseThrow(() -> LawNotFoundException.EXCEPTION);
+        UserVote userVote = userVoteRepository.findByUserId(user.getId());
 
         Comment comment = Comment.builder()
                 .commentType(request.getCommentType())
+                .voteType(userVote.getVoteType())
                 .content(request.getContent())
                 .user(user)
                 .law(law)
